cb8367b33d765c759bac7e1b1f73abfbe060281e
InoutSide <inout.side@hotmail.com>
2020-10-01 18:20:53 +0300

    Изменение интерфейса 1.0

--- FamilyBudget/Form2.Designer.cs
+++ FamilyBudget/Form2.Designer.cs
@@ -28,19 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(523, 315);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(552, 13);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(471, 315);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(552, 346);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(471, 350);
+            this.dataGridView3.TabIndex = 2;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(733, 500);
+            this.ClientSize = new System.Drawing.Size(1035, 754);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
     }
 }
--- FamilyBudget/Form2.cs
+++ FamilyBudget/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,63 @@ namespace FamilyBudget
 {
     public partial class Form2 : Form
     {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FamilyDbN;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        SqlConnection sqlConnection;
+        SqlCommand sqlCommand;
+        SqlDataAdapter sqlDataAdapter;
+        DataTable Expenses;
+        DataTable Incomes;
+
+        BindingSource bs;
+
         public Form2()
         {
             InitializeComponent();
+            //Первый способ
+            ConnectToTypeExpense();
+            //Второй способ
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            string commandStr = "SELECT E.Id, T.Title, E.Amount, E.Date FROM Expenses AS E JOIN TypeExpense AS T On E.ExpenseType = T.Id";
+            sqlCommand = new SqlCommand(commandStr, sqlConnection);
+            sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            Expenses = new DataTable();
+            sqlDataAdapter.Fill(Expenses);
+            dataGridView1.DataSource = Expenses;
+
+            //Среднее расходов
+            double Sum = 0, Sr;
+            foreach (DataRow person in Expenses.Rows)
+            {
+                Sum = Sum + Convert.ToDouble(person["Amount"]);
+            }
+
+            Sr = Sum / Expenses.Rows.Count;
+            textBox5.Text = Convert.ToString(Sr);
+
+            //Третий способ
+            string ComString = "SELECT I.Id, FM.Name, I.Amount, I.Date  FROM Incomes AS I JOIN FamilyMembers As FM ON I.Earned = FM.Id";
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(ComString, connectionString);
+
+            var IncomesTable = new DataTable();
+            dataAdapter.Fill(IncomesTable);
+            bs = new BindingSource();
+            bs.DataSource = IncomesTable;
+            dataGridView3.DataSource = bs;           
+        }
+        //Первый способ
+        private void ConnectToTypeExpense()
+        {
+            string ComString = "SELECT * FROM TypeExpense";
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(ComString, connectionString);
+
+            var ExpensesTable = new DataTable();
+            dataAdapter.Fill(ExpensesTable);
+            dataGridView2.DataSource = ExpensesTable;
         }
     }
 }
